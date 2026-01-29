a472b904e201da1e7ba07649220e584de561d565
Darius <dartig@alu.edu.gva.es>
2026-01-29 20:44:18 +0100

    feat: implemented custom exception for http responses

--- src/main/kotlin/com/rapidex/rapidex_mobile_api/service/OrderService.kt
+++ src/main/kotlin/com/rapidex/rapidex_mobile_api/service/OrderService.kt
@@ -1,6 +1,8 @@
 package com.rapidex.rapidex_mobile_api.service
 
 import com.rapidex.rapidex_mobile_api.entities.Order
+import com.rapidex.rapidex_mobile_api.exceptions.OrderNotFoundException
+import com.rapidex.rapidex_mobile_api.exceptions.PendingOrderNotFoundException
 import com.rapidex.rapidex_mobile_api.repositories.EmployeeRepository
 import com.rapidex.rapidex_mobile_api.repositories.OrderRepository
 import org.springframework.stereotype.Service
@@ -17,7 +19,24 @@ class OrderService(
         *  2- dont have prep_date
         *  3- dont have dispatch_date
         * -- "dont have" means its null-- */
-        fun getAllOrders(): List<Order> = orderRepository.getAllOrders()
-        fun getPendingOrders(): List<Order> = orderRepository.getPendingOrders()
+
+        fun getAllOrders(): List<Order> {
+                val orders = orderRepository.getAllOrders()
+                if (orders.isEmpty()) {
+                        throw OrderNotFoundException("table 'orders' is empty")
+                }
+
+                return orders
+        }
+
+
+        fun getPendingOrders(): List<Order> {
+                val pendingOrders = orderRepository.getPendingOrders()
+                if (pendingOrders.isEmpty()) {
+                        throw PendingOrderNotFoundException("there is no order with an 'id_emp' and 'prep_date' ")
+                }
+
+                return pendingOrders
+        }
 
 }
