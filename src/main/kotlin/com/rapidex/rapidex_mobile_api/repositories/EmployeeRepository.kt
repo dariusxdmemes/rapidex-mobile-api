248731903201e07b4388e904d4d85f784a30f473
Darius <dartig@alu.edu.gva.es>
2026-02-04 19:34:55 +0100

    task: employee repo and entity w/ username & password

--- src/main/kotlin/com/rapidex/rapidex_mobile_api/repositories/EmployeeRepository.kt
+++ src/main/kotlin/com/rapidex/rapidex_mobile_api/repositories/EmployeeRepository.kt
@@ -3,4 +3,6 @@ package com.rapidex.rapidex_mobile_api.repositories
 import com.rapidex.rapidex_mobile_api.entities.Employee
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface EmployeeRepository : JpaRepository<Employee, Int>
+interface EmployeeRepository : JpaRepository<Employee, Int> {
+    fun findByUsername(username: String): Employee?
+}
