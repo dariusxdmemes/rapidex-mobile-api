0f1ae9f8c52bdd70ebdccb30fc20d57a43fd6c59
Darius <dartig@alu.edu.gva.es>
2026-02-04 19:59:35 +0100

    feat: employee-login

---
+++ src/main/kotlin/com/rapidex/rapidex_mobile_api/controllers/EmployeeRestController.kt
@@ -0,0 +1,23 @@
+package com.rapidex.rapidex_mobile_api.controllers
+
+import com.rapidex.rapidex_mobile_api.dto.EmployeeDTO
+import com.rapidex.rapidex_mobile_api.model.LoginRequestModel
+import com.rapidex.rapidex_mobile_api.service.EmployeeService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/employees")
+class EmployeeRestController(
+    private val service: EmployeeService
+) {
+
+    @PostMapping("/login")
+    fun login(
+        @RequestBody request: LoginRequestModel
+    ): EmployeeDTO {
+        return service.login(request)
+    }
+}
