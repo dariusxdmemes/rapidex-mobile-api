ae7ed4c1bbc205fe5541ef6489dba261679df791
Darius <dartig@alu.edu.gva.es>
2026-02-06 20:05:01 +0100

    task: index view and list employees view

--- src/main/kotlin/com/rapidex/rapidex_mobile_api/controllers/web/RapidexWebDashController.kt
+++ src/main/kotlin/com/rapidex/rapidex_mobile_api/controllers/web/RapidexWebDashController.kt
@@ -1,2 +1,17 @@
-package com.rapidex.rapidex_mobile_api.controllers.web 
+package com.rapidex.rapidex_mobile_api.controllers.web
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/rapidex")
+class RapidexWebDashController {
+
+    @GetMapping
+    fun index(): String {
+
+        return "index"
+    }
+}
